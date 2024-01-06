9aa51f9ef7c4f9257459c390c573a2950de7e275
Juan Urdaneta <jurdanetac10@gmail.com>
2024-01-06 18:40:18 -0400

    Refactor blog router to its own module

--- index.js
+++ index.js
@@ -6,26 +6,14 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const Blog = require('./models/blogs');
+const blogsRouter = require('./controllers/blogs');
+app.use('/api/blogs', blogsRouter);
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
