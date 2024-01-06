066c9acb05ffc798ebad0fdc181a8112ae6cb505
Juan Urdaneta <jurdanetac10@gmail.com>
2024-01-06 18:50:08 -0400

    Finish refactoring app

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const express = require('express');
-
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const Blog = require('./models/blogs');
-const blogsRouter = require('./controllers/blogs');
-app.use('/api/blogs', blogsRouter);
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
--- utils/middleware.js
+++ utils/middleware.js
@@ -17,7 +17,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
-  } if (error.name === 'ValidationError') {
+  }
+  if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
   }
 
